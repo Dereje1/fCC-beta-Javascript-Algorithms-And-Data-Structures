b5c283d40c4fd70d6bd397011da1765da64c3d0f
Dereje Getahun <getahun.d@gmail.com>
2018-05-21 19:10:31 -0400

    Update cashregister.js

--- cashregister.js
+++ cashregister.js
@@ -1,4 +1,4 @@
-  const currencyUnit={
+ const currencyUnit={
     "PENNY": 0.01, 
     "NICKEL": 0.05, 
     "DIME": 0.1,
@@ -17,11 +17,12 @@ function checkCashRegister(price, cash, cid) {
   let usableCurrencyUnits=[];//build list of all units that can be used to give change
   for (let unit in currencyUnit){
     if(totalChange>=currencyUnit[unit]){
-      const maxUnit = cid.filter((c)=>{
+      //filter to find how much of the unit is avalialble in the register
+      const cidUnit = cid.filter((c)=>{
         return c[0]===unit;
-      })[0][1]; //how much of the unit is avalialble in the register
-      if(maxUnit>0){
-        usableCurrencyUnits.push([unit,currencyUnit[unit],maxUnit]);
+      });
+      if(cidUnit.length>0 && cidUnit[0][1]>0){
+        usableCurrencyUnits.push([unit,currencyUnit[unit],cidUnit[0][1]]);
       }
     }
   }
@@ -52,10 +53,10 @@ function getChange(allUnits,fullChangeRequired){
   function unitChange(units,changeRequired){//recursive unit change could use further refactoring
     let changeCounter=0; //counter for a particular unit
     while(true){
-      const nextIncrementTest = (changeCounter+1)*units[0][1];//total change at next unit increment
+      const nextIncrementTest = (changeCounter+1)*units[0][1]
       const changeFullfilled = nextIncrementTest > changeRequired;
       const availableReached = nextIncrementTest > units[0][2];
-      if(changeFullfilled || availableReached){
+      if(changeFullfilled||availableReached){
           if(changeCounter*units[0][1]!=0){
             builtChange.push([units[0][0],changeCounter*units[0][1]]);
           }
